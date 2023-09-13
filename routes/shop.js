adbb4c5a207a00fb2504d5de352e747e8bebd1f6
ashishmodi <ashishmodi@fno.co>
2023-09-13 19:20:57 +0530

    Using template engine Pug

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,15 @@ const express = require('express')
 const path = require('path');
 const router = express.Router();
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {      // app.use is an alternative
     // res.send("<h1>Hello from Express / !</h1>");
-    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));   // /views or ./views will not work, ../ to go level up. Also, .. still works, skip /    
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));   // /views or ./views will not work, ../ to go level up. Also, .. still works, skip /
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    console.log(adminData.products);
+    res.render('shop', {pageTitle: 'Shop', prods: products, path: '/'});   // use default templating engine
 });
 
 module.exports = router;
